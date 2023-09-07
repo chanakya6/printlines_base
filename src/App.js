2a84dd3387f1ca9327978f769da77fb18b2a964f
Tejaswini21 <yechurichanakya@gmail.com>
2023-09-07 19:53:00 +0530

    config changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>PRINTLINES</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.printlines.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          PRINTLINES
         </a>
       </header>
     </div>
